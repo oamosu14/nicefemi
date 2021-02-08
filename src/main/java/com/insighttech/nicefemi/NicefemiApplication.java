74eeacf937db4533220f227742fa44885d7e81b5
student <nicefemi@yahoo.com>
2021-02-08 19:34:43 +0000

    initial commit

---
+++ src/main/java/com/insighttech/nicefemi/NicefemiApplication.java
@@ -0,0 +1,13 @@
+package com.insighttech.nicefemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicefemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicefemiApplication.class, args);
+	}
+
+}
